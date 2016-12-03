9b38aa2dc298b01794d53f08888251369200576a
Casey Metz <caseymetz@gmail.com>
2016-12-03 13:41:48 -0700

    initial commit, add router.js constructor

---
+++ router.js
@@ -0,0 +1,19 @@
+let Router = module.exports = () => {
+  this.routes = [];
+};
+
+Router.prototype.add = (method, url, handler) => this.routes.push({ method, url, handler });
+
+Router.prototype.resolve = (request, response) => {
+  let path = require('url').parse(request.url).pathname;
+
+  return this.routes.some(route => {
+    let match = route.url.exec(path);
+    if (!match || route.method != request.method){ return false; }
+
+    let urlParts = match.slice(1).map(decodeURIComponent);
+    route.handler.apply(null, [request, response].concat(urlParts));
+
+    return true;
+  });
+};
